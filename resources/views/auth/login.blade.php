e18a83e2633f10c715fb7d71f27c7be5b8f6ecfd
Prakash <shadyprakash8@gmail.com>
2018-12-16 18:49:46 -0800

    login using github

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,6 +57,11 @@
                                     {{ __('Login') }}
                                 </button>
 
+                                <a href="{{ url('login/github') }}"><button type="button" class="btn btn-primary">
+                                    {{ __('Github') }}
+                                </button></a>
+
+
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
